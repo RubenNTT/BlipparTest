57c41b44fddbfb877e0b3bc7c7e1530ce4d49a5d
Ruben Parma <rparmaia@emeal.nttdata.com>
2024-06-26 19:11:11 +0200

    [FIX] Fix chache put 2

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -24,10 +24,12 @@ self.addEventListener('fetch', function (e) {
       console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
       if (response) { return response; }
 
-      response = await fetch(e.request, {cache: "no-store"});
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
+      response = await fetch(e.request);
+	  if(e.method == "GET){
+		  const cache = await caches.open(cacheName);
+		  console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+		  cache.put(e.request, response.clone());
+	  }
       return response;
     })());
 });
